9c81ac4ee39c79cd04a5fa3a5c6117dc982f3a19
Mariana Cousseiro de Lima <limatainer@MBP-de-Mariana.lan>
2022-10-09 12:42:36 +0100

    form and about us updates

--- frontweb/src/Page/Formulario/index.jsx
+++ frontweb/src/Page/Formulario/index.jsx
@@ -3,23 +3,26 @@ import mao from '../../assets/img/mao.png';
 export default function Formulario() {
 
   return (
-    <>
-      <div className="container flex flex-row items-center">
-        <span className='text-center font-light text-doacao-300'><p>Não precisamos de seu contato pessoal nem dados,
-          nós nunca iremos pedir isto. Neste formulario compartilhe a história de alguém que precisa
-          de uma dessas doações. Precisaremos apenas do
-          contacto da pessoa indicada e se der tudo certo avançaremos ou não com a doação</p></span>
-        <img src={mao} alt="mao" className='text-center' />
+    <div div className='grid place-items-center '>
+
+      <div class="flex items-center bg-white rounded-lg border shadow-md md:flex-row md:max-w-xl hover:bg-gray-100">
+        <img class="object-cover pl-10 w-full h-96 rounded-t-lg md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src={mao} alt="" />
+        <div class="flex flex-col justify-between p-4 leading-normal">
+          <h5 class="mb-2 text-2xl font-bold tracking-tight text-doacao-300 ">Formulário</h5>
+          <p class="mb-3 font-light text-gray-700">Não precisamos de seu contato pessoal nem dados,
+            nós nunca iremos pedir isto. Neste formulario compartilhe a história de alguém que precisa
+            de uma dessas doações. Precisaremos apenas do
+            contacto da pessoa indicada e se der tudo certo avançaremos ou não com a doação.</p>
+        </div>
       </div>
-      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+      <br />
+      <div className="flex items-center bg-white rounded-lg border shadow-md md:flex-col md:max-w-xl hover:bg-gray-100">
         <div className="bg-white py-8 px-6 shadow rounded-ls sm:px-10">
-          <p className='text-sm font-normal'>Se você concorda em compartilhar a história
-            de alguém e o email desta pessoa, poderá então avançar e preencher
-            este formulário de indicações, caso a pessoa indicada não tenha email
-            pode escrever na história algum contato. <br /> Também deve concordar que sabe que isto
-            não significa que iremos automáticamente fazer a doação para a pessoa que indicou
-            mas que iremos avaliar. <br /> As doações são anônimas e sem divulgação, nem
-            mídia e nem postagens em lado algum.</p>
+          <p className='mb-3 font-light text-gray-700'>Preencha caso concorde em partilhar a história de alguém. <br />
+            Caso a pessoa indicada não tenha email, pode escrever na história outro contato. <br />
+            Ao partilhar, não significa que iremos automáticamente fazer a doação para a pessoa que indicou,
+            mas que iremos avaliar. <br />
+            As doações são anônimas e sem divulgações, nem em mídia e nem postagens em lado algum.</p>
           <br />
           <form className="mb-0 space-y-3"
             action="https://docs.google.com/forms/d/e/1FAIpQLSfkC47Gi3kLeUoAeklz0iMXHcnapFrJPpTttT9O9Ea-GIVprQ/formResponse">
@@ -31,7 +34,7 @@ export default function Formulario() {
               focus:outline-none
               focus:ring-rocho-500
               focus:border-rocho-500
-              w-full" type="text" placeholder="Nome" name="entry.1555852290" />
+              w-full" type="text" placeholder="Nome(da pessoa indicada)" name="entry.1555852290" />
             <input className="border border-doacao-300 
               rounded-md
               px-5
@@ -39,7 +42,7 @@ export default function Formulario() {
               focus:outline-none
               focus:ring-rocho-500
               focus:border-rocho-500
-              w-full" type="email" placeholder="Email" name="entry.1533199738" />
+              w-full" type="email" placeholder="Email(da pessoa indicada)" name="entry.1533199738" />
             <textarea cols="30" rows="10" className="border border-doacao-300 
               rounded-md
               px-5
@@ -57,6 +60,7 @@ export default function Formulario() {
               focus:ring-rocho-500
               focus:border-rocho-500
               w-full" type="text" placeholder="Sua assinatura" name="entry.271024323" />
+
             <div className="mt-3
               sm:mt-0
               sm:ml-3
@@ -81,9 +85,8 @@ export default function Formulario() {
             </div>
           </form>
         </div>
-        <br />
         <img src={boneco} alt="boneco" className='text-center' />
       </div>
-    </>
+    </div>
   )
 }
--- frontweb/src/Page/QuemSomos/index.tsx
+++ frontweb/src/Page/QuemSomos/index.tsx
@@ -6,7 +6,7 @@ export default function QuemSomos() {
       <div className='w-full aspect-square'>
         <img src={bonecoLegal} alt="boneco legal" />
       </div>
-      <div className='w-full text-center pr-28 shadow-md hover:shadow-doacao-300'>
+      <div className='w-full  pr-28'>
         <h1 className="mb-4 text-3xl font-extrabold text-gray-900  md:text-5xl lg:text-6xl">
           <span className="text-transparent bg-clip-text bg-gradient-to-r
            to-doacao-300 from-sky-400">Somo doadores anónimos</span></h1>
@@ -21,14 +21,15 @@ export default function QuemSomos() {
           quemos apenas ajudar, te parece estranho?
         </p>
         <p className="font-light text-rocho-500">
-          Acreditamos que dessa maneira o foco estará no que importa
-          que é a ajuda sem olhar a quem, nem a quem doou nem a quem recebeu
-          por isso não divulgaremos nossos doadores e nem a quem doamos apenas
-          pedimos historias reais de pessoas que precisam de ajuda com algo que
+          Acreditamos que dessa maneira focamos no mais importante,
+          que é ajudar sem olhar a quem. <br /> Nem a quem doou, nem a quem recebeu.
+          <br /> Por isso, não divulgaremos nossos doadores e nem a quem doaremos.
+          <br /> Precisamos de historias reais de pessoas que precisam da ajuda de algo que
           temos a disposição e se acreditarmos que é válido, esta pessoa será
-          contactada e receberá a doação. Não buscamos mídia nem nada disso,
-          somos jovens e queremos ajudar.</p>
+          contactada e receberá a doação.
+          <br /> Não buscamos mídia nem nada disso, somos jovens e queremos ajudar.</p>
       </div>
     </div>
+
   )
 }
