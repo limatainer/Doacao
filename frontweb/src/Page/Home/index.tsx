737f7699a3521061e2254e77077a1a3f29e9d0dd
Mariana Cousseiro de Lima <limatainer@MBP-de-Mariana.lan>
2022-10-09 16:02:25 +0100

    final touches

--- frontweb/src/Page/Home/index.tsx
+++ frontweb/src/Page/Home/index.tsx
@@ -4,7 +4,8 @@ import Donations from '../../components/Donations';
 export default function Home() {
   return (
     <div>
-      <h1 className='container text-center py-3'>Esta plataforma é dedicada a doações apenas dentro da região metropolitana de Recife</h1>
+      <h1 className='text-center text-transparent bg-clip-text bg-gradient-to-r
+           to-sky-400 from-doacao-300'>Esta plataforma é dedicada a doações apenas dentro da região metropolitana de Recife</h1>
       <div className='flex h-screen'>
         <div className='m-auto p-12'>
           <img src={Main} alt="Main" />
--- frontweb/src/Page/ParaDoar/index.tsx
+++ frontweb/src/Page/ParaDoar/index.tsx
@@ -1,23 +1,34 @@
-import React from 'react'
+import comida from '../../assets/img/comida.jpg';
+import fraldabebe from '../../assets/img/fraldabebe.jpg';
+import mamadeira from '../../assets/img/mamadeira.jpg';
+import higiene from '../../assets/img/higiene.jpg';
+import pc from '../../assets/img/pc.jpg';
+import idoso from '../../assets/img/idoso.jpg';
+
+import { Link } from "react-router-dom";
 
 export default function ParaDoar() {
   return (
     <>
+      <br />
+      <h3 className='text-center text-transparent bg-clip-text bg-gradient-to-r
+           to-sky-400 from-doacao-300'>Itens Disponíveis para doação</h3>
+      <br />
       <div className="container my-12 mx-auto px-4 md:px-12">
         <div className="flex flex-wrap -mx-1 lg:-mx-4">
           <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
             <article className="overflow-hidden rounded-lg shadow-lg">
-              <a href="#">
-                <img alt="Placeholder" className="block h-auto w-full" src="https://picsum.photos/600/400/?random" />
-              </a>
+              <Link to="/formulario">
+                <img alt="Placeholder" className="block h-auto w-full" src={higiene} />
+              </Link>
               <header className="flex items-center justify-between leading-tight p-2 md:p-4">
                 <h1 className="text-lg">
                   <a className="no-underline hover:underline text-black" href="#">
-                    Article Title
+                    Kit Higiene básica
                   </a>
                 </h1>
                 <p className="text-grey-darker text-sm">
-                  11/1/19
+                  01/02/23
                 </p>
               </header>
 
@@ -25,11 +36,11 @@ export default function ParaDoar() {
                 <a className="flex items-center no-underline hover:underline text-black" href="#">
                   <img alt="Placeholder" className="block rounded-full" src="https://picsum.photos/32/32/?random" />
                   <p className="ml-2 text-sm">
-                    Author Name
+                    Anónimo
                   </p>
                 </a>
                 <a className="no-underline text-grey-darker hover:text-red-dark" href="#">
-                  <span className="hidden">Like</span>
+                  <span className="hidden">Dinsponível</span>
                   <i className="fa fa-heart"></i>
                 </a>
               </footer>
@@ -39,18 +50,18 @@ export default function ParaDoar() {
           <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
             <article className="overflow-hidden rounded-lg shadow-lg">
 
-              <a href="#">
-                <img alt="Placeholder" className="block h-auto w-full" src="https://picsum.photos/600/400/?random" />
-              </a>
+              <Link to="/formulario">
+                <img alt="Placeholder" className="block h-auto w-full" src={pc} />
+              </Link>
 
               <header className="flex items-center justify-between leading-tight p-2 md:p-4">
                 <h1 className="text-lg">
                   <a className="no-underline hover:underline text-black" href="#">
-                    Article Title
+                    Notebook Usado
                   </a>
                 </h1>
                 <p className="text-grey-darker text-sm">
-                  11/1/19
+                  01/02/23
                 </p>
               </header>
 
@@ -58,11 +69,11 @@ export default function ParaDoar() {
                 <a className="flex items-center no-underline hover:underline text-black" href="#">
                   <img alt="Placeholder" className="block rounded-full" src="https://picsum.photos/32/32/?random" />
                   <p className="ml-2 text-sm">
-                    Author Name
+                    Anónimo
                   </p>
                 </a>
                 <a className="no-underline text-grey-darker hover:text-red-dark" href="#">
-                  <span className="hidden">Like</span>
+                  <span className="hidden">Dinsponível</span>
                   <i className="fa fa-heart"></i>
                 </a>
               </footer>
@@ -71,17 +82,17 @@ export default function ParaDoar() {
           </div>
           <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
             <article className="overflow-hidden rounded-lg shadow-lg">
-              <a href="#">
-                <img alt="Placeholder" className="block h-auto w-full" src="https://picsum.photos/600/400/?random" />
-              </a>
+              <Link to="/formulario">
+                <img alt="Placeholder" className="block h-auto w-full" src={comida} />
+              </Link>
               <header className="flex items-center justify-between leading-tight p-2 md:p-4">
                 <h1 className="text-lg">
                   <a className="no-underline hover:underline text-black" href="#">
-                    Article Title
+                    Cestas básicas
                   </a>
                 </h1>
                 <p className="text-grey-darker text-sm">
-                  11/1/19
+                  01/02/23
                 </p>
               </header>
 
@@ -89,11 +100,11 @@ export default function ParaDoar() {
                 <a className="flex items-center no-underline hover:underline text-black" href="#">
                   <img alt="Placeholder" className="block rounded-full" src="https://picsum.photos/32/32/?random" />
                   <p className="ml-2 text-sm">
-                    Author Name
+                    Anónimo
                   </p>
                 </a>
                 <a className="no-underline text-grey-darker hover:text-red-dark" href="#">
-                  <span className="hidden">Like</span>
+                  <span className="hidden">Dinsponível</span>
                   <i className="fa fa-heart"></i>
                 </a>
               </footer>
@@ -106,18 +117,18 @@ export default function ParaDoar() {
 
             <article className="overflow-hidden rounded-lg shadow-lg">
 
-              <a href="#">
-                <img alt="Placeholder" className="block h-auto w-full" src="https://picsum.photos/600/400/?random" />
-              </a>
+              <Link to="/formulario">
+                <img alt="Placeholder" className="block h-auto w-full" src={idoso} />
+              </Link>
 
               <header className="flex items-center justify-between leading-tight p-2 md:p-4">
                 <h1 className="text-lg">
                   <a className="no-underline hover:underline text-black" href="#">
-                    Article Title
+                    Fraudas Geriátricas
                   </a>
                 </h1>
                 <p className="text-grey-darker text-sm">
-                  11/1/19
+                  01/02/23
                 </p>
               </header>
 
@@ -125,44 +136,11 @@ export default function ParaDoar() {
                 <a className="flex items-center no-underline hover:underline text-black" href="#">
                   <img alt="Placeholder" className="block rounded-full" src="https://picsum.photos/32/32/?random" />
                   <p className="ml-2 text-sm">
-                    Author Name
+                    Anónimo
                   </p>
                 </a>
                 <a className="no-underline text-grey-darker hover:text-red-dark" href="#">
-                  <span className="hidden">Like</span>
-                  <i className="fa fa-heart"></i>
-                </a>
-              </footer>
-
-            </article>
-          </div>
-          <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
-            <article className="overflow-hidden rounded-lg shadow-lg">
-
-              <a href="#">
-                <img alt="Placeholder" className="block h-auto w-full" src="https://picsum.photos/600/400/?random" />
-              </a>
-
-              <header className="flex items-center justify-between leading-tight p-2 md:p-4">
-                <h1 className="text-lg">
-                  <a className="no-underline hover:underline text-black" href="#">
-                    Article Title
-                  </a>
-                </h1>
-                <p className="text-grey-darker text-sm">
-                  11/1/19
-                </p>
-              </header>
-
-              <footer className="flex items-center justify-between leading-none p-2 md:p-4">
-                <a className="flex items-center no-underline hover:underline text-black" href="#">
-                  <img alt="Placeholder" className="block rounded-full" src="https://picsum.photos/32/32/?random" />
-                  <p className="ml-2 text-sm">
-                    Author Name
-                  </p>
-                </a>
-                <a className="no-underline text-grey-darker hover:text-red-dark" href="#">
-                  <span className="hidden">Like</span>
+                  <span className="hidden">Dinsponível</span>
                   <i className="fa fa-heart"></i>
                 </a>
               </footer>
@@ -172,18 +150,18 @@ export default function ParaDoar() {
           <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
             <article className="overflow-hidden rounded-lg shadow-lg">
 
-              <a href="#">
-                <img alt="Placeholder" className="block h-auto w-full" src="https://picsum.photos/600/400/?random" />
-              </a>
+              <Link to="/formulario">
+                <img alt="Placeholder" className="block h-auto w-full" src={mamadeira} />
+              </Link>
 
               <header className="flex items-center justify-between leading-tight p-2 md:p-4">
                 <h1 className="text-lg">
                   <a className="no-underline hover:underline text-black" href="#">
-                    Article Title
+                    Leite NAN Pro
                   </a>
                 </h1>
                 <p className="text-grey-darker text-sm">
-                  11/1/19
+                  01/02/23
                 </p>
               </header>
 
@@ -191,11 +169,44 @@ export default function ParaDoar() {
                 <a className="flex items-center no-underline hover:underline text-black" href="#">
                   <img alt="Placeholder" className="block rounded-full" src="https://picsum.photos/32/32/?random" />
                   <p className="ml-2 text-sm">
-                    Author Name
+                    Anónimo
                   </p>
                 </a>
                 <a className="no-underline text-grey-darker hover:text-red-dark" href="#">
-                  <span className="hidden">Like</span>
+                  <span className="hidden">Dinsponível</span>
+                  <i className="fa fa-heart"></i>
+                </a>
+              </footer>
+
+            </article>
+          </div>
+          <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
+            <article className="overflow-hidden rounded-lg shadow-lg">
+
+              <Link to="/formulario">
+                <img alt="Placeholder" className="block h-auto w-full" src={fraldabebe} />
+              </Link>
+
+              <header className="flex items-center justify-between leading-tight p-2 md:p-4">
+                <h1 className="text-lg">
+                  <a className="no-underline hover:underline text-black" href="#">
+                    Fraudas Bebês
+                  </a>
+                </h1>
+                <p className="text-grey-darker text-sm">
+                  01/02/23
+                </p>
+              </header>
+
+              <footer className="flex items-center justify-between leading-none p-2 md:p-4">
+                <a className="flex items-center no-underline hover:underline text-black" href="#">
+                  <img alt="Placeholder" className="block rounded-full" src="https://picsum.photos/32/32/?random" />
+                  <p className="ml-2 text-sm">
+                    Anónimo
+                  </p>
+                </a>
+                <a className="no-underline text-grey-darker hover:text-red-dark" href="#">
+                  <span className="hidden">Dinsponível</span>
                   <i className="fa fa-heart"></i>
                 </a>
               </footer>
@@ -204,6 +215,8 @@ export default function ParaDoar() {
           </div>
         </div>
       </div>
+      <br />
+      <p className='text-center italic hover:font-light text-slate-400'>As imagens são meramente representativas</p>
     </>
   )
 }
