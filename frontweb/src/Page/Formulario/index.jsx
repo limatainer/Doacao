3df9e52ab13ed52a2aa1c96d401685850189a54c
Mariana Cousseiro de Lima <limatainer@MacBook-Pro-de-Mariana.local>
2022-10-14 19:43:32 +0100

    responsive

--- frontweb/src/Page/Formulario/index.jsx
+++ frontweb/src/Page/Formulario/index.jsx
@@ -3,9 +3,8 @@ import mao from '../../assets/img/mao.png';
 export default function Formulario() {
 
   return (
-    <div div className='grid place-items-center '>
-
-      <div class="flex items-center bg-white rounded-lg border shadow-md md:flex-row md:max-w-xl hover:bg-gray-100">
+    <div div className='grid place-items-center'>
+      <div class="flex flex-wrap items-center bg-white rounded-lg border shadow-md md:flex-row md:max-w-xl hover:bg-gray-100">
         <img class="object-cover pl-10 w-full h-96 rounded-t-lg md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src={mao} alt="" />
         <div class="flex flex-col justify-between p-4 leading-normal">
           <h5 class="mb-2 text-2xl font-bold tracking-tight text-doacao-300 ">Formulário</h5>
@@ -16,7 +15,7 @@ export default function Formulario() {
         </div>
       </div>
       <br />
-      <div className="flex items-center bg-white rounded-lg border shadow-md md:flex-col md:max-w-xl hover:bg-gray-100">
+      <div className="flex flex-wrap items-center bg-white rounded-lg border shadow-md md:flex-col md:max-w-xl hover:bg-gray-100">
         <div className="bg-white py-8 px-6 shadow rounded-ls sm:px-10">
           <p className='mb-3 font-light text-gray-700'>Preencha caso concorde em partilhar a história de alguém. <br />
             Caso a pessoa indicada não tenha email, pode escrever na história outro contato. <br />
--- frontweb/src/components/Footer/index.tsx
+++ frontweb/src/components/Footer/index.tsx
@@ -3,16 +3,10 @@ import Logo from '../../assets/img/Logologo.svg'
 export default function Footer() {
   return (
     <>
-      <footer className="w-full h-16 bg-white border-t-2
-            fixed left-0 bottom-0
-            flex justify-center items-center
-            text-doacao-300 text-2xl shadow hover:shadow-lg">
-        <div className="flex justify-center">
-          <div><p>Copyright © 2023 </p></div>
-          <div><a href="https://www.youtube.com/watch?v=lXyMhZZy4mg" className="flex mb-4 sm:mb-0">
-            <img src={Logo} className="content-center mr-3 w-28" alt="Doações Logo" />
-          </a></div>
-          <div><p>Todos os direitos reservados <span className='text-rocho-500'>Agradecemos</span></p></div>
+      <footer className="bg-gray-200 text-center lg:text-left">
+        <div className="text-doacao-300 text-center p-4">
+          © 2023 Doação :
+          <a className="text-rocho-500" href="https://tailwind-elements.com/"> Lima.Codes</a>
         </div>
       </footer>
     </>
